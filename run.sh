580d0335ed3e2821718c676a54984a51d21ac850
Lukas Kallies <kallies@puzzle.ch>
2017-03-24 13:00:00 +0100

    Publish modules in modules dir because gitlab-ci only publishes artifacts in ./modules

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 moduledir=$(mktemp -d --suffix=_modules)
 #pubdir="/var/www/html/pub/puppet-modules"
-pubdir="${PWD}/artifacts"
+pubdir="${PWD}/modules"
 
 librarian-puppet install --path ${moduledir}  --clean --destructive --verbose
 pulp-puppet-module-builder ${moduledir} --output-dir=${pubdir}
